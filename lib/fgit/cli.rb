f4d41894905f9bb1029e84b59d4e8076392438e5
yan_runchen <runchen.yan@drecom.co.jp>
2015-07-07 17:21:18 +0900

    add cp method and delete origin from branch name

--- lib/fgit/cli.rb
+++ lib/fgit/cli.rb
@@ -2,17 +2,27 @@ require "thor"
 
 module Fgit
   class CLI < Thor
-    desc "list BRANCH FILE_NAME", "list file in the given branch from origin"
-    def list(source_branch=nil, file_name)
-      branch = source_branch.nil? ? "HEAD" : "origin/#{source_branch}"
-      command= "git ls-tree -r --name-only #{branch} | grep '#{file_name}'"
+    desc "ls BRANCH FILE_NAME", "list file in the given branch"
+    def ls(source_branch=nil, file_name)
+      branch = source_branch.nil? ? "HEAD" : "#{source_branch}"
+      command= "git ls-tree -r --name-only #{branch} | grep -E '^(.*/)*#{file_name}$'"
 
       real_file_paths = `#{command}`
 
       if real_file_paths.empty?
-        puts "[Error] File:#{file_name} can not be found！"
+        puts "[Error] File: #{file_name} can not be found！"
       else
         puts real_file_paths
+      end
+      real_file_paths
+    end
+
+    desc "cp BRANCH FILE_NAME", "copy file from the given branch"
+    def cp(source_branch, file_name)
+      real_file_paths = ls(source_branch, file_name)
+      real_file_paths.split.compact.each do |file_path|
+        copy_command = "git checkout #{source_branch} #{file_path}"
+        system copy_command
       end
     end
   end
